2ca08958e478711b8db498786da6707e6d90402f
Flameofrage <lance@zeligman.net>
2017-05-31 01:35:21 -0400

    Got R2's !fish command running properly

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # R2Z2
+Just a bot that's going to cause someone a lot of trouble. 
+
+To Do:
+-Create !act which will perform an action on a specified user
--- modules/commands.rb
+++ modules/commands.rb
@@ -2,11 +2,21 @@ module R2Z2
 	module Commands
 	        extend Discordrb::Commands::CommandContainer
 		extend Discordrb::EventContainer
-		command(:ping) do |event| 
+		command(:ping, description: 'Responds with pong') do |event| 
 			event.respond "Pong!"
 		end
-		command(:invite) do |event|
+		command(:invite, description: 'Invite R2Z2 to your channel') do |event|
 			event.respond "Invite me via #{R2Z2.invite_url}"
 		end
+		command(:userlist, description: 'Lists users hopefully') do |event|
+			event.respond "Here, #{R2Z2.users}"
+		end
+
+		command(:fish, description: 'Randomly selects a user and slaps them with a trout') do |event|
+			members = event.server.online_members
+			members.reject!(&:current_bot?).map! { |m| "#{m.id}" }
+			event.respond "*slaps around <@#{members.sample}> with a large trout*"	
+		end
+
 	end
 end
