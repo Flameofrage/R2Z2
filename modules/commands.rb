9d187254f0ba27e6e3e918d971e706a8517abf20
Flameofrage <lance@zeligman.net>
2017-05-31 01:52:14 -0400

    Custom rate limit message

--- modules/commands.rb
+++ modules/commands.rb
@@ -12,7 +12,7 @@ module R2Z2
 			event.respond "Here, #{R2Z2.users}"
 		end
 
-		command(:fish, bucket: :limit, description: 'Apply directly to the forehead') do |event|
+		command(:fish, bucket: :limit, rate_limit_message: '%User%, quit being a dick for %time% more seconds.', description: 'Apply directly to the forehead') do |event|
 			members = event.server.online_members
 			members.reject!(&:current_bot?).map! { |m| "#{m.id}" }
 			event.respond "*slaps around <@#{members.sample}> with a large trout*"	
