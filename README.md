6df7b2af6edafaf9177e30b8a57f35cd81fcbe95
Lance Zeligman <lance@zeligman.net>
2017-06-09 11:03:19 -0400

    Edited Readme and improved

--- README.md
+++ README.md
@@ -12,5 +12,31 @@ Just a bot that's going to cause someone a lot of trouble.
 
   * `fish` Apply directly to the forehead
 
+  * `lmgtfy` Let me google that for you
+
+  * `roll` Rolls a number of dice for you
+
+  * `queue` shows what's in the music queue
+
+  * `repeat` repeats current song
+
+  * `skip` skips current song
+
+  * `yt` searches youtube
+
+  * `musichelp` gives help for how to use the music commands
+
+  * `clearqueue` clears the music queue of either all songs or a specified song
+
+  * `leave` Makes R2Z2 leave the music queue
+
+  * `join` Makes R2Z2 join the voice channel
+
+  * `add` adds music to the music queue
+
 ### To Do
 -Create !act which will perform an action on a specified user
+-Change behavior so that auto leave voice channel can be specified on a per server basis
+
+### Credits
+Most of the music (%99) came form the Sapphirebot project where it was released under the MIT license. So if you enjoy R2 and want to thank someone for his music commands, please drop them a line!
--- modules/commands.rb
+++ modules/commands.rb
@@ -2,156 +2,157 @@ module R2Z2
   module Commands
   module Add
     extend Discordrb::Commands::CommandContainer
-      command(:ping, bucket: :limit, description: 'Responds with pong') do |event| 
-        event.respond "Pong!"
-      end
-      
-      command(:invite, bucket: :limit, description: 'Invite R2Z2 to your channel') do |event|
-        event.respond "Invite me via #{R2Z2.invite_url}"
-      end
-      
-      command(:fish, bucket: :limit, rate_limit_message: '%User%, quit being a dick for %time% more seconds.', description: 'Apply directly to the forehead') do |event|
-        members = event.server.online_members
-        members.reject!(&:current_bot?).map! { |m| "#{m.id}" }
-        event.respond "*slaps around <@#{members.sample}> with a large trout*"	
-      end
+    command(:ping, bucket: :limit, description: 'Responds with pong') do |event| 
+      event.respond "Pong!"
+    end
+    
+    command(:invite, bucket: :limit, description: 'Invite R2Z2 to your channel') do |event|
+      event.respond "Invite me via #{R2Z2.invite_url}"
+    end
+    
+    command(:fish, bucket: :limit, rate_limit_message: '%User%, quit being a dick for %time% more seconds.', description: 'Apply directly to the forehead') do |event|
+      members = event.server.online_members
+      members.reject!(&:current_bot?).map! { |m| "#{m.id}" }
+      event.respond "*slaps around <@#{members.sample}> with a large trout*"	
+    end
 
-      command(:lmgtfy, min_args: 1, description: 'Generates Let Me Goole That For You link.', usage: 'lmgtfy <text>') do |event, *text|
-        GOOGLE.shorten_text("http://lmgtfy.com/?q=#{text.join('+')}",
-                             preview: event.server.preview,
-                             original: event.server.original,
-                             minlength: event.server.minlength)
+    command(:lmgtfy, min_args: 1, description: 'Generates Let Me Goole That For You link.', usage: 'lmgtfy <text>') do |event, *text|
+      GOOGLE.shorten_text("http://lmgtfy.com/?q=#{text.join('+')}",
+                           preview: event.server.preview,
+                           original: event.server.original,
+                           minlength: event.server.minlength)
+    end
+
+    command(:roll, description: 'Rolls a number of dice', usage: 'roll <number> <number>') do |event, number, num2|
+      event << "Rolling #{number}d#{num2}"
+      event <<  number.to_i.times.map{ Random.rand(num2.to_i) }
+    end
+
+    command(:stats, description: 'Shows bot statistics') do |event|
+      ping = ((Time.now - event.timestamp) * 1000).to_i
+      event << "\nServers: #{STATS.servers}."
+      event << "Users: #{STATS.users}."
+      event << "Times mentioned: #{STATS.mentions}."
+      event << "Uptime: #{time_in_words(STATS.uptime)}."
+      event << "Urls shortened: #{STATS.urls_shortened}."
+      event << "Youtube videos found: #{STATS.videos_found}"
+      event << "Songs played: #{STATS.songs_played}"
+      event << "Messages read: #{STATS.messages_read}."
+      event << "Ping: #{ping}ms."
+    end
+
+    command(:queue, description: 'Displays current music queue.') do |event|
+      if event.server.music_player.queue.empty?
+        'Queue is empty, use `add` to add more songs.'
+      else
+        "`#{event.server.music_player.table}`"
       end
+    end
 
-      command(:stats, description: 'Shows bot statistics') do |event|
-        ping = ((Time.now - event.timestamp) * 1000).to_i
-        event << "\nServers: #{STATS.servers}."
-        event << "Users: #{STATS.users}."
-        event << "Times mentioned: #{STATS.mentions}."
-        event << "Uptime: #{time_in_words(STATS.uptime)}."
-        event << "Urls shortened: #{STATS.urls_shortened}."
-        event << "Youtube videos found: #{STATS.videos_found}"
-        event << "Songs played: #{STATS.songs_played}"
-        event << "Messages read: #{STATS.messages_read}."
-        event << "Ping: #{ping}ms."
+    command(:repeat, description: 'Toggles repeat.', required_permissions: [:manage_server]) do |event|
+      event.server.music_player.repeat = !event.server.music_player.repeat
+      "Repeat is now #{bool_to_words(event.server.music_player.repeat)}."
+    end
+
+    command(:skip, description: 'Skips current song.', required_permissions: [:manage_server]) do |event|
+      break if event.server.music_player.queue.empty? || !event.server.music_player.playing?
+
+      event.server.music_player.skip = true
+      event.voice.stop_playing if event.voice
+      nil
+    end
+
+    command(:yt, description: 'Finds youtube videos.', min_args: 1, usage: 'yt <query>') do |_event, *query|
+      video = GOOGLE.find_video(query.join(' '))
+      if video
+        "https://youtu.be/#{video}"
+      else
+        'Such video does not exist.'
       end
+    end
 
-      command(:queue, description: 'Displays current music queue.') do |event|
-        if event.server.music_player.queue.empty?
-          'Queue is empty, use `add` to add more songs.'
-        else
-          "`#{event.server.music_player.table}`"
-        end
-      end
+    command(:musichelp, description: 'Displays information on how to use music features.') do |event|
+      event << 'To start using music bot a user with `manage server` permission has to invite it to a channel by using `join` command.'
+      event << 'Then you can add songs by using `add` command.'
+      event << 'Use `queue` command to see added songs.'
+      event << 'Users with `manage server` permission can remove songs from queue by using `clearqueue <id>` command.'
+      event << 'Each song will start playing automaticlly after the last one finishes.'
+      event << "If you're not using music bot features anymore use `leave` command."
+      event << 'You can find more help for each of these commands by using `help <commandname>` command.'
+    end
 
-      command(:repeat, description: 'Toggles repeat.', required_permissions: [:manage_server]) do |event|
-        event.server.music_player.repeat = !event.server.music_player.repeat
-        "Repeat is now #{bool_to_words(event.server.music_player.repeat)}."
-      end
-
-      command(:roll, description: 'Rolls a dice.') do
-        rand(1..6)
-      end
-
-      command(:skip, description: 'Skips current song.', required_permissions: [:manage_server]) do |event|
-        break if event.server.music_player.queue.empty? || !event.server.music_player.playing?
-
-        event.server.music_player.skip = true
-        event.voice.stop_playing if event.voice
-        nil
-      end
-
-      command(:yt, description: 'Finds youtube videos.', min_args: 1, usage: 'yt <query>') do |_event, *query|
-        video = GOOGLE.find_video(query.join(' '))
-        if video
-          "https://youtu.be/#{video}"
-        else
-          'Such video does not exist.'
-        end
-      end
-
-      command(:musichelp, description: 'Displays information on how to use music features.') do |event|
-        event << 'To start using music bot a user with `manage server` permission has to invite it to a channel by using `join` command.'
-        event << 'Then you can add songs by using `add` command.'
-        event << 'Use `queue` command to see added songs.'
-        event << 'Users with `manage server` permission can remove songs from queue by using `clearqueue <id>` command.'
-        event << 'Each song will start playing automaticlly after the last one finishes.'
-        event << "If you're not using music bot features anymore use `leave` command."
-        event << 'You can find more help for each of these commands by using `help <commandname>` command.'
-      end
-
-      command(:clearqueue, description: 'Deletes songs from server queue.', usage: 'clearqueue <index/all>', required_permissions: [:manage_server], min_args: 1) do |event, argument|
-        if argument.chomp == 'all'
-          event.server.music_player.disconnect
-        elsif argument.to_i.between?(1, event.server.music_player.queue.length)
-          index = argument.to_i - 1
-          if index.zero?
-            event.voice.stop_playing
-            event.server.music_player.repeat = false
-          else
-            event.server.music_player.delete_song_at(index)
-          end
-        elsif !argument.to_i.between?(1, event.server.music_player.queue.length)
-          next "Can't find song with such index"
-        else
-          next 'Unknown argument, use `all` or song index.'
-        end
-        nil
-      end
-
-      #Makes R2 Leave a voice channel
-      command(:leave, description: 'Makes the bot leave your voice channel.', required_permissions: [:manage_server]) do |event|
+    command(:clearqueue, description: 'Deletes songs from server queue.', usage: 'clearqueue <index/all>', required_permissions: [:manage_server], min_args: 1) do |event, argument|
+      if argument.chomp == 'all'
         event.server.music_player.disconnect
-        nil
+      elsif argument.to_i.between?(1, event.server.music_player.queue.length)
+        index = argument.to_i - 1
+        if index.zero?
+          event.voice.stop_playing
+          event.server.music_player.repeat = false
+        else
+          event.server.music_player.delete_song_at(index)
+        end
+      elsif !argument.to_i.between?(1, event.server.music_player.queue.length)
+        next "Can't find song with such index"
+      else
+        next 'Unknown argument, use `all` or song index.'
       end
-      
-      #Makes R2 join voice channel
-      command(:join, description: 'Makes the bot join your voice channel.', required_permissions: [:manage_server]) do |event|
-        channel = event.user.voice_channel
+      nil
+    end
 
-        # Check if channel is valid.
-        if !channel || channel == event.server.afk_channel
-          next 'First join a valid voice channel.'
-        end
+    #Makes R2 Leave a voice channel
+    command(:leave, description: 'Makes the bot leave your voice channel.', required_permissions: [:manage_server]) do |event|
+      event.server.music_player.disconnect
+      nil
+    end
+    
+    #Makes R2 join voice channel
+    command(:join, description: 'Makes the bot join your voice channel.', required_permissions: [:manage_server]) do |event|
+      channel = event.user.voice_channel
 
-        # Try to join the voice channel.
-        begin
-          event.bot.voice_connect(channel)
-          event.voice.encoder.use_avconv = true
-        rescue Discordrb::Errors::NoPermission
-          next 'Please make sure I have permission to join this channel.'
-        end
-
-        # Set voice object that should be used for playback.
-        event.server.music_player.voice = event.voice
-
-        # Set channel that should be used for bot responses.
-        event.server.music_player.channel = event.channel
-
-        LOGGER.debug "Music bot joined #{event.channel.id}."
-        "Joined \"#{channel.name}\". Use `add` command if you want to add songs to queue."
+      # Check if channel is valid.
+      if !channel || channel == event.server.afk_channel
+        next 'First join a valid voice channel.'
       end
 
+      # Try to join the voice channel.
+      begin
+        event.bot.voice_connect(channel)
+        event.voice.encoder.use_avconv = true
+      rescue Discordrb::Errors::NoPermission
+        next 'Please make sure I have permission to join this channel.'
+      end
 
-      # Adds a song to server queue and starts playing it.
-      command(:add, description: 'Adds a song to server queue and starts playing it.', usage: 'add <query>', min_args: 1) do |event, *query|
-        if !event.voice
-          next 'First make me join your voice channel by using `join` command.'
-        elsif event.server.music_player.queue.length >= MusicBot::MAX_SONGS_IN_QUEUE
-          next 'Music music queue is too long.'
-        end
+      # Set voice object that should be used for playback.
+      event.server.music_player.voice = event.voice
 
-        # Find the video and let user know if it does not exist.
-        query = query.join(' ')
-        video_id = GOOGLE.find_video(query)
-        next 'Such video does not exist.' unless video_id
+      # Set channel that should be used for bot responses.
+      event.server.music_player.channel = event.channel
 
-        # Download the song and add it to queue.
-        # If this succeeds then start playing it unless music is already being played.
-        if event.server.music_player.add(video_id)
-          event.server.music_player.start_loop unless event.server.music_player.playing?
-        end
-        nil
+      LOGGER.debug "Music bot joined #{event.channel.id}."
+      "Joined \"#{channel.name}\". Use `add` command if you want to add songs to queue."
+    end
+
+
+    # Adds a song to server queue and starts playing it.
+    command(:add, description: 'Adds a song to server queue and starts playing it.', usage: 'add <query>', min_args: 1) do |event, *query|
+      if !event.voice
+        next 'First make me join your voice channel by using `join` command.'
+      elsif event.server.music_player.queue.length >= MusicBot::MAX_SONGS_IN_QUEUE
+        next 'Music music queue is too long.'
+      end
+
+      # Find the video and let user know if it does not exist.
+      query = query.join(' ')
+      video_id = GOOGLE.find_video(query)
+      next 'Such video does not exist.' unless video_id
+
+      # Download the song and add it to queue.
+      # If this succeeds then start playing it unless music is already being played.
+      if event.server.music_player.add(video_id)
+        event.server.music_player.start_loop unless event.server.music_player.playing?
+      end
+      nil
       end
     end
   end
