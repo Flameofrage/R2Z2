0bbab521a4dba62dd34fef54cc3696192f9c415e
Lance Zeligman <lance@zeligman.net>
2017-07-18 09:31:06 -0700

    event testing

--- modules/events.rb
+++ modules/events.rb
@@ -1,3 +1,10 @@
-R2Z2.message(with_text: 'Ping!') do |event|
+Module R2Z2
+  Module Events
+    Module Ping
+      extend Discordrb::EventContainer
+	R2Z2.message(with_text: 'Ping!') do |event|
 	event.respond 'Pong!'
+      end
+    end
+  end
 end
