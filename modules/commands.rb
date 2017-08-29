714f2a833b23aeb38238f7d5876c6f7c8a53e55e
lanceofwhichwedream <lance.zeligman@gmail.com>
2017-08-29 19:42:56 -0400

    trying to fix lmgtfy

--- modules/commands.rb
+++ modules/commands.rb
@@ -17,10 +17,8 @@ module R2Z2
     end
 
     command(:lmgtfy, min_args: 1, description: 'Generates Let Me Goole That For You link.', usage: 'lmgtfy <text>') do |event, *text|
-      GOOGLE.shorten_text("http://lmgtfy.com/?q=#{text.join('+')}",
-                           preview: event.server.preview,
-                           original: event.server.original,
-                           minlength: event.server.minlength)
+      GOOGLE.shorten_text("http://lmgtfy.com/?q=#{text.join('+')}"
+                           )
 			$stats[:lmgtfy_links] += 1
     end
 
