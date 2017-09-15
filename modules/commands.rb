db345b973759dbe16b320ae1a41893125428e977
lanceofwhichwedream <lance.zeligman@gmail.com>
2017-09-14 21:06:26 -0400

    twitch tweaks

--- modules/commands.rb
+++ modules/commands.rb
@@ -33,7 +33,7 @@ module R2Z2
 			end 
 		end
 
-    $timer.cron '*/15 * * * *' do
+    $timer.cron '*/10 * * * *' do
       message = $streamer_hash.keys.map do |key|
         streamer = R2Z2Twitch.new(key)
 				streamer.IDLookUp
