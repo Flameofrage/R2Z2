899d77ff813c547ffc58faad157ee53eaed567c8
Lance Zeligman <lance@Eurus.warbase.xyz>
2017-12-09 23:32:54 -0500

    p s

--- r2z2.rb
+++ r2z2.rb
@@ -110,8 +110,8 @@ module R2Z2
 	# Bot Config file
 	CONFIG = YAML.load_file('data/config.yaml')
   
-  #Rufus Scheduler
-  $timer = Rufus::Scheduler.new
+        #Rufus Scheduler
+        $timer = Rufus::Scheduler.new
 
 	# Twitch Client-ID
 	$twitch_client_id = CONFIG['twitch_client_id']
