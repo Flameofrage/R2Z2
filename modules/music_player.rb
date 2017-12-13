89123f9e490ee97457c5b7dee8e987d9c9f0956b
Lance Zeligman <lance.zeligman@gmail.com>
2017-12-13 14:42:10 -0500

    wooo more typos

--- modules/music_player.rb
+++ modules/music_player.rb
@@ -96,7 +96,7 @@ module R2Z2
       # Sets Volume and saves it to the Server's hash
       def volume(volume, server)
         new_volume = { server => { 'volume' => volume }}
-        if STREAM_DATA.stream_data[server]['volume'].exists?
+        unless STREAM_DATA.stream_data[server]['volume'].nil?
           m = STREAM_DATA.stream_data[server].merge!(new_volume) { |_key, left, right| left.merge!(right) }
           STREAM_DATA.update(m)
         else
