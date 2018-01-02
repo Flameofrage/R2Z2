70e16a5226c098a0976bcb011239684a9fff5bd6
lanceofwhichwedream <lance.zeligman@gmail.com>
2018-01-02 03:15:01 -0500

    Converting event.server.id to int

--- modules/commands/addstreamer.rb
+++ modules/commands/addstreamer.rb
@@ -3,7 +3,7 @@ module R2Z2
     module AddStreamer
       extend Discordrb::Commands::CommandContainer
       command(:addstreamer, description: 'Adds a streamer', usage: 'addstreamer <username>', min_args: 1) do |event, name|
-        if (name.is_a? String) and !(STREAM_DATA.stream_data[event.server.id]["streamers"].include? name)
+        if (name.is_a? String) and !(STREAM_DATA.stream_data[event.server.id.to_i]["streamers"].include? name)
           STREAMER.IDLookUp(name)
           STREAMER.AddStreamer(event.server.id)
           event << "I've added " + name.downcase + " to the list of streamers"
