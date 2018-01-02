474e13a8ec15841dd41c386e4ed38fab384e4d63
lanceofwhichwedream <lance.zeligman@gmail.com>
2018-01-02 02:24:37 -0500

    Fixed the addstreamer issue, needed to check for the server, not the mass streamer list

--- modules/commands/addstreamer.rb
+++ modules/commands/addstreamer.rb
@@ -3,7 +3,7 @@ module R2Z2
     module AddStreamer
       extend Discordrb::Commands::CommandContainer
       command(:addstreamer, description: 'Adds a streamer', usage: 'addstreamer <username>', min_args: 1) do |event, name|
-        if (name.is_a? String) and !(STREAMER_HASH.streamer_hash.include? name.downcase)
+        if (name.is_a? String) and !(STREAM_DATA.stream_data[server]["streamers"].include? name)
           STREAMER.IDLookUp(name)
           STREAMER.AddStreamer(event.server.id)
           event << "I've added " + name.downcase + " to the list of streamers"
