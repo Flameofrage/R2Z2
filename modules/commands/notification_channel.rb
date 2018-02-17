ea3df441dc5590fe4224650891d3b3090cc6b563
lanceofwhichwedream <lance.zeligman@gmail.com>
2018-02-17 03:29:15 -0500

    Fixing NC

--- modules/commands/notification_channel.rb
+++ modules/commands/notification_channel.rb
@@ -3,8 +3,8 @@ module R2Z2
     module Volume
       extend Discordrb::Commands::CommandContainer
       command(:twitchchannel, description: 'Sets the twitch notification channel for R2.', usage: 'twitchchannel <channel id>', min_args: 1) do |event, channel|
-        STREAMER.nc(channel, event.server.id)
-        event << "Twitch notifications channel has been set to <channel>"
+        STREAMER.nc(channel.to_i, event.server.id)
+        event << "Twitch notifications channel has been set to <@#{channel}>"
       end
     end
   end
