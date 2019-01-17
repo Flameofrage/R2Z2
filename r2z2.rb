fed3f54f5ff841232ad573bef01f1402117f302f
lanceofwhichwedream <lance.zeligman@gmail.com>
2019-01-16 19:00:10 -0500

    Coding practices clean up for R2

--- r2z2.rb
+++ r2z2.rb
@@ -12,78 +12,78 @@ require 'json'
 
 Bundler.require(:default)
 
+# Methods pulled from sapphire_bot just in case
 
-#Methods pulled from sapphire_bot just in case
-
-module Kernel
 # Runs a block with warning messages supressed.
-	def run_supressed(&block)
-		original_verbosity = $VERBOSE
-		$VERBOSE = nil
-		yield block
-		$VERBOSE = original_verbosity
-	end
+module Kernel
+  def run_supressed(&block)
+    original_verbosity = $VERBOSE
+    $VERBOSE = nil
+    yield block
+    $VERBOSE = original_verbosity
+  end
 
-# Converts seconds to human readable format.
-	def time_in_words(time)
-		days = (time / 86_400).to_i
-		time -= days * 86_400
-		hours = (time / 3600).to_i
-		time -= hours * 3600
-		minutes = (time / 60).to_i
-		string = "#{days} day#{'s' unless days == 1},"
-		string << " #{hours} hour#{'s' unless hours == 1},"
-		string << " #{minutes} minute#{'s' unless minutes == 1}"
-	end
+  # Converts seconds to human readable format.
+  def time_in_words(time)
+    days = (time / 86_400).to_i
+    time -= days * 86_400
+    hours = (time / 3600).to_i
+    time -= hours * 3600
+    minutes = (time / 60).to_i
+    string = "#{days} day#{'s' unless days == 1},"
+    string << " #{hours} hour#{'s' unless hours == 1},"
+    string << " #{minutes} minute#{'s' unless minutes == 1}"
+  end
 
-# Converts boolean values to more appealing format.
-# Possible modes: on, enabled.
-	def bool_to_words(bool, mode = :on)
-		case mode
-		when :on
-			string_if_true = 'on'
-			string_if_false = 'off'
-		when :enabled
-			string_if_true = 'enabled'
-			string_if_false = 'disabled'
-		else
-			raise ArgumentError
-		end
+  # Converts boolean values to more appealing format.
+  def bool_to_words(bool, mode = :on)
+    case mode
+    when :on
+      string_if_true = 'on'
+      string_if_false = 'off'
+    when :enabled
+      string_if_true = 'enabled'
+      string_if_false = 'disabled'
+    else
+      raise ArgumentError
+    end
 
-		return string_if_true if bool
-			string_if_false
-	end
+    return string_if_true if bool
+    string_if_false
+  end
 
-	# Returns true if specified url string is valid.
-	def valid_url?(url)
-		uri = URI.parse(url)
-		return true if uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
-		false
-	rescue
-		false
-	end
+  # Returns true if specified url string is valid.
+  def valid_url?(url)
+    uri = URI.parse(url)
+    return true if uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+    false
+    rescue
+      false
+  end
 
   def notification_update(server)
-    notifications = { server => { 'previews' => 1 }}
+    notifications = { server => { 'previews' => 1 } }
       unless STREAM_DATA.stream_data[notifications].nil?
         STREAM_DATA.update
       end
   end
 
-	# Returns urls host.
-	def url_host(url)
-		URI.parse(url).host
-	rescue
-		nil
-	end
+  # Returns urls host.
+  def url_host(url)
+    URI.parse(url).host
+  rescue
+    nil
+  end
 
-# Generates a random string with default length of 10.
-	def rand_string(length = 10)
-		rand(36**length).to_s(36)
-	end
+  # Generates a random string with default length of 10.
+  def rand_string(length = 10)
+    rand(36**length).to_s(36)
+  end
 end
 
+# Begin Defining R2's actual code
 module R2Z2
+  # Define the store data module for writing to yaml
   module StoreData
     def save_to_file(file, object)
       File.open(file, 'w') do |f|
@@ -109,7 +109,7 @@ module R2Z2
   # Load Music Extension Modules
   Dir['music_bot/extensions/*.rb'].each { |mod| load mod }
 
-  # Streamer List 
+  # Streamer List
   STREAMER_HASH = StreamerHash.new
 
   # Stream Data
@@ -141,19 +141,17 @@ module R2Z2
   log_streams = [STDOUT]
 
   if debug
-  	timestamp = Time.now.strftime(Discordrb::LOG_TIMESTAMP_FORMAT).tr(':', '-')
-  	log_file = File.new("#{Dir.pwd}/logs/#{timestamp}.log", 'a+')
-  	log_streams.push(log_file)
+    timestamp = Time.now.strftime(Discordrb::LOG_TIMESTAMP_FORMAT).tr(':', '-')
+    log_file = File.new("#{Dir.pwd}/logs/#{timestamp}.log", 'a+')
+    log_streams.push(log_file)
   end
- 
-  run_supressed { LOGGER = Discordrb::LOGGER = Discordrb::Logger.new(nil, log_streams) }
- 
-  LOGGER.debug = true if debug
 
+  run_supressed { LOGGER = Discordrb::LOGGER = Discordrb::Logger.new(nil, log_streams) }
+
+  LOGGER.debug = true if debug
 
   # Stats Variable
   $stats = YAML.load_file("#{Dir.pwd}/data/stats.yaml")
-
 
   # Commands
   module DiscordCommands; end
@@ -162,15 +160,12 @@ module R2Z2
     R2Z2.include_commands DiscordCommands.const_get mod
   end
 
-  #modules.each { |m| R2Z2.include! m; puts "Included: #{m}" }
-
   GOOGLE = GoogleServices.new
-  # STATS = Stats.new
 
-	#Limit bucket
-	R2Z2.bucket :limit, limit: 6, time_span: 60, delay: 10
+  # Limit bucket
+  R2Z2.bucket :limit, limit: 6, time_span: 60, delay: 10
 
-	#Run this noise
-	R2Z2.run :async
-	R2Z2.sync
+  # Run this noise
+  R2Z2.run :async
+  R2Z2.sync
 end
